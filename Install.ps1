eb821466ff9b3e7ea066348e85cc6e95986a51e4
Muhammad bin Yusrat <ibnyusrat@gmail.com>
2022-09-27 02:45:59 -0700

    Updated Install Script.

--- Install.ps1
+++ Install.ps1
@@ -1,40 +1,77 @@
+Write-Host "Installing Windows Terminal.."
+winget install 9N0DX20HK701 -s msstore --accept-package-agreements --accept-source-agreements
+
+Write-Host "Installing PowerShell:"
+winget install 9MZ1SNWT0N5D -s msstore --accept-package-agreements --accept-source-agreements
+
 Write-Host "Installing oh-my-posh from Microsoft Store"
 winget install oh-my-posh -s msstore --accept-package-agreements --accept-source-agreements
 winget install JanDeDobbeleer.OhMyPosh -s winget --accept-package-agreements --accept-source-agreements
 
+Write-Host "Initialize oh-my-posh"
 oh-my-posh init pwsh | Invoke-Expression
 
+Write-Host "Fetch oh-my-posh themes:"
 Get-PoshThemes
 
-if (!(Test-Path $PROFILE))
-{
-   New-Item -Path $PROFILE -Type File -Force
-   Write-Host "Created empty file for Powershell Profile"
-}
-Add-Content -Path $PROFILE "oh-my-posh init pwsh | Invoke-Expression"
-Add-Content -Path $PROFILE -value "Add-Alias open explorer.exe"
-#Font-Installation Script credit: https://gist.github.com/anthonyeden/0088b07de8951403a643a8485af2709b
-
-$SourceDir   = ".\fonts\"
-$Source      = ".\fonts\*"
+Write-Host "Installing fonts.."
+$Source = ".\fonts\*"
 $Destination = (New-Object -ComObject Shell.Application).Namespace(0x14)
-$TempFolder  = "C:\Windows\Temp\Fonts"
+$TempFolder = "C:\Windows\Temp\Fonts"
 
 New-Item $TempFolder -Type Directory -Force | Out-Null
 
-Get-ChildItem -Path $Source -Include '*.ttf','*.ttc','*.otf' -Recurse | ForEach {
-    If (-not(Test-Path "C:\Windows\Fonts\$($_.Name)")) {
-
-        $Font = "$TempFolder\$($_.Name)"
-        
-        # Copy font to local temporary folder
-        Copy-Item $($_.FullName) -Destination $TempFolder
-        
-        # Install font
-        $Destination.CopyHere($Font,0x10)
-
-        # Delete temporary copy of font
+# Install Fonts
+Get-ChildItem -Path $Source -Include '*.ttf', '*.ttc', '*.otf' -Recurse | ForEach {
+    If (-not(Test-Path "C:\Windows\Fonts\$( $_.Name )"))
+    {
+        $Font = "$TempFolder\$( $_.Name )"
+        Copy-Item $( $_.FullName ) -Destination $TempFolder
+        $Destination.CopyHere($Font, 0x10)
         Remove-Item $Font -Force
     }
 }
 
+Write-Host "Updating PowerShell Profile";
+
+if (!(Test-Path $PROFILE))
+{
+    New-Item -Path $PROFILE -Type File -Force
+    Write-Host "Created empty file for Powershell Profile (it didn't exist..)"
+}
+
+Add-Content -Path $PROFILE "oh-my-posh init pwsh --config '$env:POSH_THEMES_PATH\hunk.omp.json' | Invoke-Expression"
+Add-Content -Path $PROFILE -value "Set-Alias open explorer.exe"
+
+Write-Host "Update Windows Terminal Config..";
+$PowerShellJSONPath = "$env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
+
+$PowerShellSettings = Get-Content $PowerShellJSONPath -raw | ConvertFrom-Json
+$PowerShellProfileId = "";
+$PowerShellSettings.profiles.list | % {
+    if ($_.name -eq 'PowerShell')
+    {
+        $_.font = {
+            face: "MesloLGS NF"
+        }
+        $_.useAcrylic = $True
+        $_.opacity = 60
+        $PowerShellProfileId = $_.guid
+    }
+}
+
+$PowerShellSettings.defaultProfile = $PowerShellProfileId;
+$PowerShellSettings.useAcrylicInTabRow = $True;
+
+$PowerShellSettings | ConvertTo-Json -depth 32| set-content $PowerShellJSONPath
+
+Write-Output $PowerShellSettings
+
+Clear-Host;
+
+Write-Host "If everything went well, you should be good to go. Just exist Windows PowerShell and launch Windows Terminal from the start menu.";
+Write-Host "";
+
+Write-Host -NoNewLine 'Press any key to exit Windows Powershell...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+exit
---
+++ README.md
@@ -0,0 +1,8 @@
+## My Windows Terminal
+This repository contains a file called `Install.ps1`. This will set up the terminal for you. 
+You don't need to run with elevated privileges (`Run as Administrator`) but before running the script,
+should allow execution of remote scripts with the following command (and it requires admin privileges):
+
+`Set-ExecutionPolicy RemoteSigned`
+
+And this needs to be run with administrative privileges.
