129b69ccf04a89354e373a9c3efaeb5fcdf4c0ba
Muhammad bin Yusrat <ibnyusrat@gmail.com>
2023-11-01 02:55:59 +0500

    Added auto install of jq.

--- Install_Step1.ps1
+++ Install_Step1.ps1
@@ -8,6 +8,9 @@ Write-Host "Installing oh-my-posh.."
 winget install oh-my-posh -s msstore --accept-package-agreements --accept-source-agreements
 winget install JanDeDobbeleer.OhMyPosh -s winget --accept-package-agreements --accept-source-agreements
 
+Write-Host "Installing jq"
+winget install jqlang.jq
+
 Write-Host "Please exit Windows PowerShell and Launch Windows Terminal from Start Menu. Then Run Install_Step2 to continue the setup.";
 
 Write-Host -NoNewLine 'Press any key to continue..';
